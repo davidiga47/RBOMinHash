1e81fb59e62e5031e13c3a4553361280fd9c3d73
davidiga47 <davidiga47@gmail.com>
2026-03-28 22:06:17 +0100

    Added results_mean.py

---
+++ examples/results/ann_experiment/results_mean.py
@@ -0,0 +1,50 @@
+
+def avg_res(l):
+    return sum(l) / len(l) if l else 0
+
+def compute_avgs():
+    precision = []
+    recall = []
+    ratio = []
+    
+    building_time = []
+    hashing_time = []
+    query_lsh = []
+    query_rbo = []
+    memory = []
+    
+    with open("5_neighbors.txt") as f:
+        for line in f:
+            if "MEAN PRECISION" in line:
+                precision.append(float(line.split(":")[1]))
+            elif "MEAN RECALL" in line:
+                recall.append(float(line.split(":")[1]))
+            elif "MEAN RATIO" in line:
+                ratio.append(float(line.split(":")[1]))
+    
+            elif "MEAN BUILDING TIME OF THE LSH SCHEMES" in line:
+                building_time.append(float(line.split(":")[1].split()[0]))
+            elif "MEAN HASHING TIME" in line:
+                hashing_time.append(float(line.split(":")[1].split()[0]))
+            elif "MEAN QUERY TIME FOR LSH" in line:
+                query_lsh.append(float(line.split(":")[1].split()[0]))
+            elif "MEAN QUERY TIME FOR RBO" in line:
+                query_rbo.append(float(line.split(":")[1].split()[0]))
+            elif "MEAN MEMORY UTILIZATION" in line:
+                memory.append(float(line.split(":")[1].split()[0]))
+    
+    print("PRECISION:", avg_res(precision))
+    print("RECALL:", avg_res(recall))
+    print("RATIO (RBO/p.collisions):", avg_res(ratio))
+    
+    print("BUILDING TIME FOR THE LSH:", avg_res(building_time))
+    print("HASHING TIME:", avg_res(hashing_time))
+    print("QUERY TIME WITH LSH:", avg_res(query_lsh))
+    print("QUERY TIME WITHOUT LSH:", avg_res(query_rbo))
+    print("MEMORY UTILIZATION BY LSH:", avg_res(memory))
+    
+
+if __name__=="__main__":
+    print("\n")
+    compute_avgs()
+    print("\n")
